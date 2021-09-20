4967f62fdd1e03ffdcc3ee5687c792b5ca22499d
godfgod1 <godfgod1@naver.com>
2021-09-20 22:33:23 +0900

    css turn

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from flask import Flask,render_template, request,redirect,send_file
 from  index import export_combine_jobs
+from  datas.save import save_file
 
 app = Flask('remote_job_searching')
-
+db = {}
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -11,9 +12,41 @@ def home():
 @app.route('/result')
 def result():
     result = request.args.get('job')
-    jobs = export_combine_jobs(result)
-    count = len(jobs)
-    return render_template('result.html',result=result,jobs=jobs,count=count)
+    try:
+         
+        if result:
+            result = result.lower()
+            searched_job = db.get(result)
+            if searched_job:
+                jobs = []
+                jobs = searched_job
+            else:
+                jobs = export_combine_jobs(result)
+                db[result] = jobs
+        else:
+            raise Exception()
+    except:
+        return redirect('/error')
+    return render_template('result.html',result=result,jobs=jobs,count=len(jobs))
+
+@app.route('/export')
+def export_file():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_file(jobs)
+        return send_file('jobs.csv')
+    except:
+        return redirect('/error')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 
 app.run('')
